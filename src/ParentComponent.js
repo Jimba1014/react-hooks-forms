6351478a58d376b4ee5286f5455c3d14e5c7adf3
Jim <jimba.ed@outlook.com>
2022-09-22 16:19:10 -0400

    here are my changes

---
+++ src/ParentComponent.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Form from "./components/Form";
+import DisplayData from "./components/DisplayData";
+
+function ParentComponent() {
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
+  
+    function handleFirstNameChange(e) {
+      setFirstName(e.target.value);
+    }
+    function handleLastNameChange(e) {
+      setLastName(e.target.value);
+     
+}
+
+    return (
+        <div>
+            <Form 
+            firstName={firstName} 
+            lastName={lastName}
+            handleFirstNameChange={handleFirstNameChange}
+            handleLastNameChange={handleLastNameChange}
+            />
+            <DisplayData firstName={firstName} lastName={lastName} />
+        </div>
+        
+    )}
+export default ParentComponent
